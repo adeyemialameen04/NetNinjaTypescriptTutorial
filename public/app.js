7f298b3801dac01dbf3ab06700192e4c582e226a
Al Ameen Adeyemi <adeyemialameen04@gmail.com>
2023-08-25 19:12:20 +0100

    classes

--- public/app.js
+++ public/app.js
@@ -1,4 +1,21 @@
 "use strict";
+// classes
+class Invoice {
+    constructor(c, d, a) {
+        this.client = c;
+        this.amount = a;
+        this.details = d;
+    }
+    format() {
+        return `${this.client} owes $${this.amount} for ${this.details}`;
+    }
+}
+const invOne = new Invoice("mario", "work on the mario", 240);
+const invTwo = new Invoice("farhan", "work on the farhan", 24);
+let invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+console.log(invoices);
 const form = document.querySelector(".new-item-form");
 const type = document.querySelector("#type");
 const tofrom = document.querySelector("#tofrom");
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,28 @@
+// classes
+class Invoice {
+    client: string;
+    details: string;
+    amount: number;
+
+    constructor(c: string, d: string, a: number) {
+        this.client = c
+        this.amount = a
+        this.details = d
+    }
+    format() {
+        return `${this.client} owes $${this.amount} for ${this.details}`
+    }
+}
+
+const invOne = new Invoice("mario", "work on the mario", 240)
+const invTwo = new Invoice("farhan", "work on the farhan", 24)
+
+let invoices: Invoice[] = []
+invoices.push(invOne)
+invoices.push(invTwo)
+
+console.log(invoices)
+
 const form = document.querySelector(".new-item-form") as HTMLFormElement
 
 const type = document.querySelector("#type") as HTMLSelectElement
