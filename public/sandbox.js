386c8cadbe78dfb36f2ce82dfafcdb5964f74322
Al Ameen Adeyemi <adeyemialameen04@gmail.com>
2023-08-25 16:39:31 +0100

    function basics

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,15 @@
 "use strict";
-console.log("test");
+let greet;
+greet = () => {
+    console.log("hello");
+};
+const add = (a, b, c = 10) => {
+    console.log(a + b);
+    console.log(c);
+};
+add(3, 89);
+const minus = (a, b) => {
+    return a - b;
+};
+const result = minus(10, 70);
+console.log(result);
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,19 @@
-console.log("test")
+let greet: Function
+
+greet = () => {
+    console.log("hello")
+}
+
+const add = (a: number, b: number, c: number | string = 10) => {
+    console.log(a + b)
+    console.log(c)
+}
+
+add(3, 89)
+
+const minus = (a: number, b: number) => {
+    return a - b
+}
+
+const result = minus(10, 70)
+console.log(result)
