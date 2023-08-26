32db78de49837f5db5f39d2004772dbba6712ee4
Al Ameen Adeyemi <adeyemialameen04@gmail.com>
2023-08-26 01:18:35 +0100

    finished the project

--- public/app.js
+++ public/app.js
@@ -1,4 +1,5 @@
 import { Invoice } from "./classes/Invoice.js";
+import { ListTemplate } from "./classes/ListTemplate.js";
 import { Payment } from "./classes/Payment.js";
 // let docOne: HasFormatter
 // let docTwo: HasFormatter
@@ -20,14 +21,62 @@ const type = document.querySelector("#type");
 const tofrom = document.querySelector("#tofrom");
 const details = document.querySelector("#details");
 const amount = document.querySelector("#amount");
+// list template
+const ul = document.querySelector("ul");
+const list = new ListTemplate(ul);
 form.addEventListener("submit", (e) => {
     e.preventDefault();
+    let values = [tofrom.value, details.value, amount.valueAsNumber];
     let doc;
     if (type.value === "invoice") {
-        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+        doc = new Invoice(...values);
     }
     else {
-        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+        doc = new Payment(...values);
     }
-    console.log(doc);
+    list.render(doc, type.value, "end");
 });
+// GENERICS
+const appUID = (obj) => {
+    let uid = Math.floor(Math.random() * 100);
+    return Object.assign(Object.assign({}, obj), { uid });
+};
+let docUID = appUID({ name: "yoshi", age: 49 });
+const docInterface = {
+    uid: 1,
+    resourceName: "person",
+    data: { name: "Hello" }
+};
+const docFour = {
+    uid: 2,
+    resourceName: "shopping",
+    data: ["test", "bread"]
+};
+// console.log(docInterface, docFour)
+// Enums
+var ResourceType;
+(function (ResourceType) {
+    ResourceType[ResourceType["BOOK"] = 0] = "BOOK";
+    ResourceType[ResourceType["AUTHOR"] = 1] = "AUTHOR";
+    ResourceType[ResourceType["FILM"] = 2] = "FILM";
+    ResourceType[ResourceType["DIRECTOR"] = 3] = "DIRECTOR";
+    ResourceType[ResourceType["PERSON"] = 4] = "PERSON";
+})(ResourceType || (ResourceType = {}));
+const docEnumsOne = {
+    uid: 1,
+    resourceType: ResourceType.AUTHOR,
+    data: { title: "Name of wind" }
+};
+const docEnumsTwo = {
+    uid: 10,
+    resourceType: ResourceType.PERSON,
+    data: { title: "Name of KK" }
+};
+// console.log(docEnumsOne)
+// console.log(docEnumsTwo)
+// TUPLES
+let arr = ["ryu", 25, true];
+let tupExam = ["ryu", 25, false];
+// console.log(tupExam)
+let student;
+student = ["Chun", 26];
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import { Invoice } from "./classes/Invoice.js"
+import { ListTemplate } from "./classes/ListTemplate.js";
 import { Payment } from "./classes/Payment.js";
 import { HasFormatter } from "./interfaces/HasFormatter.js";
 
@@ -30,15 +31,81 @@ const tofrom = document.querySelector("#tofrom") as HTMLInputElement
 const details = document.querySelector("#details") as HTMLInputElement
 const amount = document.querySelector("#amount") as HTMLInputElement
 
+// list template
+const ul = document.querySelector("ul")!
+const list = new ListTemplate(ul)
+
 form.addEventListener("submit", (e: Event) => {
     e.preventDefault()
 
+    let values: [string, string, number] = [tofrom.value, details.value, amount.valueAsNumber]
     let doc: HasFormatter
     if (type.value === "invoice") {
-        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber)
+        doc = new Invoice(...values)
     } else {
-        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber)
+        doc = new Payment(...values)
     }
 
-    console.log(doc)
+    list.render(doc, type.value, "end")
 })
+
+// GENERICS
+const appUID = <T extends { name: string }>(obj: T) => {
+    let uid = Math.floor(Math.random() * 100)
+    return { ...obj, uid }
+}
+
+let docUID = appUID({ name: "yoshi", age: 49 })
+// console.log(docUID.uid)
+
+// with interface
+interface Resource<T> {
+    uid: number,
+    resourceName: string,
+    data: T
+}
+
+const docInterface: Resource<object> = {
+    uid: 1,
+    resourceName: "person",
+    data: { name: "Hello" }
+}
+
+const docFour: Resource<string[]> = {
+    uid: 2,
+    resourceName: "shopping",
+    data: ["test", "bread"]
+}
+
+// console.log(docInterface, docFour)
+
+// Enums
+enum ResourceType { BOOK, AUTHOR, FILM, DIRECTOR, PERSON }
+interface ResourceEnums<T> {
+    uid: number,
+    resourceType: number,
+    data: T
+}
+
+const docEnumsOne: ResourceEnums<object> = {
+    uid: 1,
+    resourceType: ResourceType.AUTHOR,
+    data: { title: "Name of wind" }
+}
+const docEnumsTwo: ResourceEnums<object> = {
+    uid: 10,
+    resourceType: ResourceType.PERSON,
+    data: { title: "Name of KK" }
+}
+
+// console.log(docEnumsOne)
+// console.log(docEnumsTwo)
+
+// TUPLES
+let arr = ["ryu", 25, true]
+
+let tupExam: [string, number, boolean] = ["ryu", 25, false]
+// console.log(tupExam)
+
+let student: [string, number]
+student = ["Chun", 26]
